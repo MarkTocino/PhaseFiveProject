c07b92ae7b329f5d8c2e4b068d8f129b5e7d23f9
MarkJohnTocino <Mark_tocino@yahoo.com>
2023-09-12 09:39:50 -0400

    Use This one

--- client/jdm-media-app/app/page.js
+++ client/jdm-media-app/app/page.js
@@ -8,24 +8,24 @@ export const home = () => {
 
   return (
     <>
-    <div>
+    <div className="flexbox">
       <section className={anton.className}>
         <div
-          className="bg-cover bg-[url('../Images/Background_homepage.jpg')]">
+          className="bg-[url('../Images/Background_homepage.jpg')] bg-cover">
           <h1 className="grid place-items-center h-screen text-white text-8xl">Welcome to JDM Social Media App!</h1>
-          <div className="box">
+          <div className="flex justify-center place-items-center content-center">
             <Link href="Signup">
-              <Button className="h-12 px-6 text-2xl" type="Click" >SIGN UP</Button>
+              <Button className="flex h-12 px-6 text-2xl relative bottom-80" type="Click" >SIGN UP</Button>
             </Link>
             <Link href="Login">
-              <Button className="h-12 px-6 text-2xl">LOGIN</Button>
+              <Button className="flex h-12 px-6 text-2xl relative bottom-80">LOGIN</Button>
             </Link>
-            <div className="turboLoading">
-              <Button className="h-20 px-10 text-3xl bg-red-600 opacity-100" isLoading>TURBO LOADING...SIGN IN IS NEEDED</Button>
+
+          </div>
+          <div className="flex justify-center">
+              <Button className="h-20 px-10 text-3xl bg-red-600 opacity-100 flex text-center relative bottom-80" isLoading>TURBO LOADING...SIGN IN IS NEEDED</Button>
             </div>
           </div>
-
-        </div>
       </section>
     </div>
     </>
